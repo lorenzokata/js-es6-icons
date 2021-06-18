a83b2f4b817dec60a938b828223e5923dd62ae6b
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-18 11:59:46 +0200

    milestone 2

---
+++ script.js
@@ -0,0 +1,140 @@
+const icons = [
+    {
+      name: 'cat',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'crow',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dog',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dove',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dragon',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'horse',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'hippo',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'fish',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'carrot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'apple-alt',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'lemon',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'pepper-hot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'user-astronaut',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-graduate',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-ninja',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-secret',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+  ];
+
+
+icons.forEach(obj => {
+
+    document.getElementById('row').innerHTML += `
+    
+    <div class="col">
+        <div class="icon-card p-2 rounded bg-white d-flex flex-column justify-content-center align-items-center">
+            <i class="${obj.family} ${obj.prefix}${obj.name} d-block fs-1"></i>
+            <div class="icon-title text-uppercase">${obj.name}</div>
+        </div>
+    </div>
+
+    `
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+<div class="col">
+    <div class="icon-card p-2 rounded bg-white d-flex flex-column justify-content-center align-items-center">
+        <i class="fas fa-cat d-block fs-1"></i>
+        <div class="icon-title text-uppercase">cat</div>
+    </div>
+</div>
+`
