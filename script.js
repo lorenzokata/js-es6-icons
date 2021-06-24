012a4654fa38f776c4d5a25870e52c4f77f2120a
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-24 18:45:55 +0200

    fine milestone 3

--- script.js
+++ script.js
@@ -120,8 +120,48 @@ const iconsColor = icons.map((obj) => {
 
 });
 
+printIcons(iconsColor);
 
-iconsColor.forEach(obj => {
+// Milestone 3 Creare un select per il tipo di icona
+
+const type = [];
+
+iconsColor.forEach(element => {
+
+  if (!type.includes(element.type)) {
+    type.push(element.type);
+    document.getElementById('type').innerHTML +=
+    `
+    <option value="${element.type}">${element.type}</option>
+    `
+  }
+
+});
+
+const select = document.getElementById('type');
+
+select.addEventListener("change", function(){
+
+  const valueSelect = select.value;
+
+  const filteredIcons = iconsColor.filter((element) => {
+    return element.type == valueSelect;
+  });
+  printIcons(filteredIcons);
+  if (valueSelect == 'All') {
+    printIcons(iconsColor);
+  }
+
+});
+
+
+// funzione di stampa icons
+
+function printIcons(array) {
+
+  document.getElementById('row').innerHTML = "";
+
+  array.forEach(obj => {
 
     document.getElementById('row').innerHTML += `
     
@@ -135,37 +175,4 @@ iconsColor.forEach(obj => {
     `
 });
 
-// Milestone 3 Creare un select per il tipo di icona
-
-const type = [];
-
-iconsColor.forEach(element => {
-
-  if (!type.includes(element.type)) {
-    type.push(element.type);
-    document.getElementById('type').innerHTML +=
-    `
-    <option value="element.type">${element.type}</option>
-    `
-  }
-
-});
-
-
-
-
-
-
-
-
-
-
-
-`
-<div class="col">
-    <div class="icon-card p-2 rounded bg-white d-flex flex-column justify-content-center align-items-center">
-        <i class="fas fa-cat d-block fs-1"></i>
-        <div class="icon-title text-uppercase">cat</div>
-    </div>
-</div>
-`
+}
