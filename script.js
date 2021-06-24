cb2687676f676dbaeeb9878faa19508db5c58277
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-24 17:15:27 +0200

    add milestone 3

--- script.js
+++ script.js
@@ -97,14 +97,37 @@ const icons = [
     },
   ];
 
+// Milestone 2 Coloriamo le icone per tipo
 
-icons.forEach(obj => {
+const iconsColor = icons.map((obj) => {
+
+    let color;
+    switch (obj.type) {
+        case 'animal':
+            color = '#ff00ff'
+            break;
+    
+        case 'vegetable':
+            color = '#ff0000'
+            break;
+            
+        case 'user':
+            color = '#0000ff'
+            break;
+    }
+
+    return {...obj , color}
+
+});
+
+
+iconsColor.forEach(obj => {
 
     document.getElementById('row').innerHTML += `
     
     <div class="col">
         <div class="icon-card p-2 rounded bg-white d-flex flex-column justify-content-center align-items-center">
-            <i class="${obj.family} ${obj.prefix}${obj.name} d-block fs-1"></i>
+            <i class="${obj.family} ${obj.prefix}${obj.name} d-block fs-1" style="color: ${obj.color}"></i>
             <div class="icon-title text-uppercase">${obj.name}</div>
         </div>
     </div>
@@ -112,13 +135,21 @@ icons.forEach(obj => {
     `
 });
 
+// Milestone 3 Creare un select per il tipo di icona
 
+const type = [];
 
+iconsColor.forEach(element => {
 
+  if (!type.includes(element.type)) {
+    type.push(element.type);
+    document.getElementById('type').innerHTML +=
+    `
+    <option value="element.type">${element.type}</option>
+    `
+  }
 
-
-
-
+});
 
 
 
